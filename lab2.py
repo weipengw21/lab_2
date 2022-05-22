e7268c967e93d99e89dbd4155ae8d837f1f5639c
weipeng <{weipengw11.21@ichat.sp.edu.sg}>
2022-05-22 21:12:05 +0800

    exercise 2

--- lab2.py
+++ lab2.py
@@ -1,1 +1,25 @@
-print("ET0735(DevOps for Alot) -lab 2 - introduction to pyhton")
+def display_main_menu():
+    print('Enter some numbers separated by commas (e.g. 5,67, 32)')
+
+
+def get_user_input():
+    num_list = []
+
+    str = input()
+
+    str_list = str.split(",")
+
+    for item in str_list:
+        num_list.append(int(item))
+
+    return num_list
+
+
+def main():
+    display_main_menu()
+    num_list = get_user_input()
+    print(num_list)
+    print(str(type(num_list[0])))
+
+if __name__ == "__main__":
+    main()
